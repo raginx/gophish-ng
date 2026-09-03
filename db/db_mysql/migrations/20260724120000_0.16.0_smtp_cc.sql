618ce1a1662e7b366151b8fa29c80a514dc7fa64
Reinhard Westerholt <reinhard@westerholt.me>
2026-09-03 18:43:06 +0200

    Fixed incompatibility with MySql and DEFAULT in smtp db migration (#50)

--- db/db_mysql/migrations/20260724120000_0.16.0_smtp_cc.sql
+++ db/db_mysql/migrations/20260724120000_0.16.0_smtp_cc.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE smtp ADD COLUMN cc text DEFAULT '';
+ALTER TABLE smtp ADD COLUMN cc text;
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
