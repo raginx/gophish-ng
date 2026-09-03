ce01bff5be17368955f814e1b372ac69f5c1f41b
Renaud11232 <gaspardrenaud@hotmail.com>
2026-09-03 15:24:56 +0200

    docker: allow reading all configuration properties from environment variables

--- docker/run.sh
+++ docker/run.sh
@@ -1,64 +1,88 @@
 #!/bin/bash
 
 # set config for admin_server
-if [ -n "${ADMIN_LISTEN_URL+set}" ] ; then
+if [ -n "${ADMIN_SERVER_LISTEN_URL+set}" ] ; then
     jq -r \
-        --arg ADMIN_LISTEN_URL "${ADMIN_LISTEN_URL}" \
-        '.admin_server.listen_url = $ADMIN_LISTEN_URL' config.json > config.json.tmp && \
+        --arg ADMIN_SERVER_LISTEN_URL "${ADMIN_SERVER_LISTEN_URL}" \
+        '.admin_server.listen_url = $ADMIN_SERVER_LISTEN_URL' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
-if [ -n "${ADMIN_USE_TLS+set}" ] ; then
+if [ -n "${ADMIN_SERVER_USE_TLS+set}" ] ; then
     jq -r \
-        --argjson ADMIN_USE_TLS "${ADMIN_USE_TLS}" \
-        '.admin_server.use_tls = $ADMIN_USE_TLS' config.json > config.json.tmp && \
+        --argjson ADMIN_SERVER_USE_TLS "${ADMIN_SERVER_USE_TLS}" \
+        '.admin_server.use_tls = $ADMIN_SERVER_USE_TLS' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
-if [ -n "${ADMIN_CERT_PATH+set}" ] ; then
+if [ -n "${ADMIN_SERVER_CERT_PATH+set}" ] ; then
     jq -r \
-        --arg ADMIN_CERT_PATH "${ADMIN_CERT_PATH}" \
-        '.admin_server.cert_path = $ADMIN_CERT_PATH' config.json > config.json.tmp && \
+        --arg ADMIN_SERVER_CERT_PATH "${ADMIN_SERVER_CERT_PATH}" \
+        '.admin_server.cert_path = $ADMIN_SERVER_CERT_PATH' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
-if [ -n "${ADMIN_KEY_PATH+set}" ] ; then
+if [ -n "${ADMIN_SERVER_KEY_PATH+set}" ] ; then
     jq -r \
-        --arg ADMIN_KEY_PATH "${ADMIN_KEY_PATH}" \
-        '.admin_server.key_path = $ADMIN_KEY_PATH' config.json > config.json.tmp && \
+        --arg ADMIN_SERVER_KEY_PATH "${ADMIN_SERVER_KEY_PATH}" \
+        '.admin_server.key_path = $ADMIN_SERVER_KEY_PATH' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
-if [ -n "${ADMIN_TRUSTED_ORIGINS+set}" ] ; then
+if [ -n "${ADMIN_SERVER_TRUSTED_ORIGINS+set}" ] ; then
     jq -r \
-        --arg ADMIN_TRUSTED_ORIGINS "${ADMIN_TRUSTED_ORIGINS}" \
-        '.admin_server.trusted_origins = ($ADMIN_TRUSTED_ORIGINS|split(","))' config.json > config.json.tmp && \
+        --arg ADMIN_SERVER_TRUSTED_ORIGINS "${ADMIN_SERVER_TRUSTED_ORIGINS}" \
+        '.admin_server.trusted_origins = ($ADMIN_SERVER_TRUSTED_ORIGINS|split(","))' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
 
 # set config for phish_server
-if [ -n "${PHISH_LISTEN_URL+set}" ] ; then
+if [ -n "${PHISH_SERVER_LISTEN_URL+set}" ] ; then
     jq -r \
-        --arg PHISH_LISTEN_URL "${PHISH_LISTEN_URL}" \
-        '.phish_server.listen_url = $PHISH_LISTEN_URL' config.json > config.json.tmp && \
+        --arg PHISH_SERVER_LISTEN_URL "${PHISH_SERVER_LISTEN_URL}" \
+        '.phish_server.listen_url = $PHISH_SERVER_LISTEN_URL' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
-if [ -n "${PHISH_USE_TLS+set}" ] ; then
+if [ -n "${PHISH_SERVER_USE_TLS+set}" ] ; then
     jq -r \
-        --argjson PHISH_USE_TLS "${PHISH_USE_TLS}" \
-        '.phish_server.use_tls = $PHISH_USE_TLS' config.json > config.json.tmp && \
+        --argjson PHISH_SERVER_USE_TLS "${PHISH_SERVER_USE_TLS}" \
+        '.phish_server.use_tls = $PHISH_SERVER_USE_TLS' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
-if [ -n "${PHISH_CERT_PATH+set}" ] ; then
+if [ -n "${PHISH_SERVER_CERT_PATH+set}" ] ; then
     jq -r \
-        --arg PHISH_CERT_PATH "${PHISH_CERT_PATH}" \
-        '.phish_server.cert_path = $PHISH_CERT_PATH' config.json > config.json.tmp && \
+        --arg PHISH_SERVER_CERT_PATH "${PHISH_SERVER_CERT_PATH}" \
+        '.phish_server.cert_path = $PHISH_SERVER_CERT_PATH' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
-if [ -n "${PHISH_KEY_PATH+set}" ] ; then
+if [ -n "${PHISH_SERVER_KEY_PATH+set}" ] ; then
     jq -r \
-        --arg PHISH_KEY_PATH "${PHISH_KEY_PATH}" \
-        '.phish_server.key_path = $PHISH_KEY_PATH' config.json > config.json.tmp && \
+        --arg PHISH_SERVER_KEY_PATH "${PHISH_SERVER_KEY_PATH}" \
+        '.phish_server.key_path = $PHISH_SERVER_KEY_PATH' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
 
-# set contact_address
+# set config for global settings
+if [ -n "${DB_NAME+set}" ] ; then
+    jq -r \
+        --arg DB_NAME "${DB_NAME}" \
+        '.db_name = $DB_NAME' config.json > config.json.tmp && \
+        cat config.json.tmp > config.json
+fi
+if [ -n "${DB_PATH+set}" ] ; then
+    jq -r \
+        --arg DB_PATH "${DB_PATH}" \
+        '.db_path = $DB_PATH' config.json > config.json.tmp && \
+        cat config.json.tmp > config.json
+fi
+if [ -n "${SECRET_KEY+set}" ] ; then
+    jq -r \
+        --arg SECRET_KEY "${SECRET_KEY}" \
+        '.secret_key = $SECRET_KEY' config.json > config.json.tmp && \
+        cat config.json.tmp > config.json
+fi
+if [ -n "${MIGRATIONS_PREFIX+set}" ] ; then
+    jq -r \
+        --arg MIGRATIONS_PREFIX "${MIGRATIONS_PREFIX}" \
+        '.migrations_prefix = $MIGRATIONS_PREFIX' config.json > config.json.tmp && \
+        cat config.json.tmp > config.json
+fi
 if [ -n "${CONTACT_ADDRESS+set}" ] ; then
     jq -r \
         --arg CONTACT_ADDRESS "${CONTACT_ADDRESS}" \
@@ -66,18 +90,17 @@ if [ -n "${CONTACT_ADDRESS+set}" ] ; then
         cat config.json.tmp > config.json
 fi
 
-# db_name has to be changed to mysql for mysql connection to work
-if [ -n "${DB_NAME+set}" ] ; then
+# set config for logging
+if [ -n "${LOGGING_FILENAME+set}" ] ; then
     jq -r \
-        --arg DB_NAME "${DB_NAME}" \
-        '.db_name = $DB_NAME' config.json > config.json.tmp && \
+        --arg LOGGING_FILENAME "${LOGGING_FILENAME}" \
+        '.logging.filename = $LOGGING_FILENAME' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
-
-if [ -n "${DB_FILE_PATH+set}" ] ; then
+if [ -n "${LOGGING_LEVEL+set}" ] ; then
     jq -r \
-        --arg DB_FILE_PATH "${DB_FILE_PATH}" \
-        '.db_path = $DB_FILE_PATH' config.json > config.json.tmp && \
+        --arg LOGGING_LEVEL "${LOGGING_LEVEL}" \
+        '.logging.level = $LOGGING_LEVEL' config.json > config.json.tmp && \
         cat config.json.tmp > config.json
 fi
 
